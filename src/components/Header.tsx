6674c2c8fc67e207687b807f05900e07e7ae517a
AloeOLOLOe <st.seregin@gmail.com>
2022-04-13 21:53:43 +0300

    14

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import  s from './Header.module.css';
 export const Header=()=>{
     return(
-        <header className='header'>
-            <img src=" https://static.vecteezy.com/system/resources/thumbnails/002/871/080/small/owl-birds-logo-free-vector.jpg"/>
+        <header className={s.header}>
+            <img className={s.header} src=" https://static.vecteezy.com/system/resources/thumbnails/002/871/080/small/owl-birds-logo-free-vector.jpg"/>
         </header>
     )
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import s from'./Profile.module.css'
+
+
 
 export const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img
                     src='https://st2.depositphotos.com/1035350/7670/i/950/depositphotos_76708101-stock-photo-macon-georgia-in-spring.jpg'/>
@@ -15,9 +18,13 @@ export const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>Post 1</div>
-                    <div>Post 2</div>
+                <div className={s.post}>
+                    <div className={s.item}>
+                        Post 1
+                    </div>
+                    <div className={s.item}>
+                        Post 2
+                    </div>
                 </div>
             </div>
         </div>
